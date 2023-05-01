b4b0cc6b322c0bf67382f5e4777e361944f3a587
MinjiKim0316 <minjikim.6420011@gmail.com>
2023-05-01 16:11:48 +0900

    update api

--- plug-in/minjikim - react/react/my-app/src/components/TodoList/TodoList.jsx
+++ plug-in/minjikim - react/react/my-app/src/components/TodoList/TodoList.jsx
@@ -8,12 +8,19 @@ export default function TodoList() {
     {id: 'list3', text: 'Do the laundry', status: 'active'},
   ]);
   const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) => setTodos(todos, map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
   return (
     <section>
       <ul>
         {
           todos.map((item) => (
-            <li key={item.id}>{item.text}</li>
+            <Todo 
+              key={item.id} 
+              todo={item} 
+              onUpdate={handleUpdate} 
+              onDelete={handleDelete}
+            />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd}/>
