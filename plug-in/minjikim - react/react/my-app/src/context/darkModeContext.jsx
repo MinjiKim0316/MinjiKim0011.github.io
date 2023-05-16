dbb036bd27bbb8614cda7a8b578b4e5e4acd020a
MinjiKim0316 <minjikim.6420011@gmail.com>
2023-05-16 14:37:32 +0900

    update

--- plug-in/minjikim - react/react/my-app/src/context/darkModeContext.jsx
+++ plug-in/minjikim - react/react/my-app/src/context/darkModeContext.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { Children, createContext } from "react";
 
@@ -11,6 +12,14 @@ export function darkModeProvider({Children}) {
     setDarkMode(!darkMode);
     updateDarkMode(!darkMode);
   };
+
+  useEffect(()=>{
+    const isDark = 
+      localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+    }, []);
+    
   return (
     <darkModeContext.Provider value={{darkMode, toggleDarkMode}}>{Children}</darkModeContext.Provider>
   ); 
@@ -19,8 +28,10 @@ export function darkModeProvider({Children}) {
 function updateDarkMode(darkMode) {
   if(darkMode) {
     document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
   } else {
     document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
   }
 }
 export const useDarkMode = () => useContext(darkModeContext);
