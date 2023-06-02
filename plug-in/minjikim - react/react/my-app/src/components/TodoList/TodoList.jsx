f7409c78a93aefc2ad8e7a7852b4c9e2fe7d6640
MinjiKim0316 <minjikim.6420011@gmail.com>
2023-06-02 11:53:08 +0900

    update

--- plug-in/minjikim - react/react/my-app/src/components/TodoList/TodoList.jsx
+++ plug-in/minjikim - react/react/my-app/src/components/TodoList/TodoList.jsx
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 export default function TodoList({filter}) {
-  const [todos, setTodos] = useState([
-    {id: 'list1', text: 'Go shopping for cooking', status: 'active'},
-    {id: 'list2', text: 'Vacuum the room', status: 'active'},
-    {id: 'list3', text: 'Do the laundry', status: 'active'},
-  ]);
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) => setTodos(todos, map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const filtered = getFilteredItems(todos, filter);
   return (
@@ -30,6 +30,11 @@ export default function TodoList({filter}) {
       <AddTodo onAdd={handleAdd}/>
     </section>
   );
+}
+
+function readTodosFromLocalStorage() {
+  const todos =localStorage.getItem('todos');
+  todos ? JSON.parse(todos) : [];
 }
 
 function getFilteredItems(todos, filter) {
